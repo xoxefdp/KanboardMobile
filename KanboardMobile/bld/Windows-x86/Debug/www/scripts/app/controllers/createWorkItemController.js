ea4d4dd93a6f278bfeeee096f37e71941e2a62a4
Harry Lucas <Harry Lucas@KATANA>
2016-12-31 15:34:46 +1030

    Switched Javascript Promises for Angular $q based promises to enable cross platform compatability.

---
+++ KanboardMobile/bld/Windows-x86/Debug/www/scripts/app/controllers/createWorkItemController.js
@@ -0,0 +1,35 @@
+﻿app
+  .controller('createWorkItemController', ['$scope', 'WorkItemService', 'ProjectService', '$location', function ($scope, WorkItemService, ProjectService, $location) {
+
+
+      ProjectService.getCurrentProjectBoard().then(function (response) {
+          $scope.board_details = response;
+          initialise();
+          $scope.$apply();
+      });
+
+      ProjectService.getCurrentProjectUsers().then(function (response) {
+          $scope.userList = response;
+          $scope.$apply();
+      });
+
+      $scope.submitChanges = function () {
+          if ($scope.newItem.title != '' && $scope.newItem.swimlane_id != null
+              && $scope.newItem.duedate != null && $scope.newItem.description != ''
+              && $scope.newItem.duedate != '') {
+              WorkItemService.createWorkItem($scope.newItem.title, $scope.newItem.owner_id, $scope.newItem.description, $scope.newItem.date_due, $scope.newItem.column_id, $scope.newItem.swimlane_id).then(function (response) {
+                  $scope.$apply();
+              });
+          }
+      }
+
+      function initialise() {
+          $scope.newItem = {
+              title: '',
+              description: '',
+              owner_id: null,
+              date_due: new Date(),
+              column_id: null
+          };
+      }
+  }]);
---
+++ KanboardMobile/bld/Windows-x86/Debug/www/scripts/app/controllers/mainScreenController.js
@@ -0,0 +1,13 @@
+﻿app
+  .controller('mainScreenController', ['$scope', 'ProjectService', '$location', function ($scope, ProjectService, $location) {
+
+      ProjectService.getMyProjects().then(function (response) {
+          $scope.projects = response;
+          $scope.$apply();
+      });
+
+      $scope.openProject = function (project) {
+          ProjectService.setCurrentProject(project.id);
+          $location.path('/workitemlist');
+      }
+  }]);
