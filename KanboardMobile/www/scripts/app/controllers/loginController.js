0d854340e1ffbb1fa82829c5e2538af11a42ac43
José Diaz <xoxefdp@gmail.com>
2019-07-09 08:21:20 +0200

    Fixed storage for protocol combo

--- KanboardMobile/www/scripts/app/controllers/loginController.js
+++ KanboardMobile/www/scripts/app/controllers/loginController.js
@@ -16,20 +16,23 @@
 
           $scope.username = "";
           $scope.password = "";
-          $scope.protocol = $scope.protocols[1];
+          $scope.protocol = {};
           $scope.server = "";
           $scope.remember_me = false;
           recallCredentials();
       }
 
       $scope.login = function () {
-          AuthenticationService.authenticate($scope.username, $scope.password, $scope.protocol.label + $scope.server).then(function (response) {
+          AuthenticationService.authenticate($scope.username, $scope.password, $scope.protocol.label + $scope.server)
+          .then(
+            function (response) {
               if ($scope.remember_me)
                 storeCredentials();
               $location.path('/mainscreen');
-          }, function (error) {
+            }, function (error) {
               navigator.notification.alert('Username or Password is incorrect', null);
-          });
+            }
+          );
       };
 
       $scope.remember_me_clicked = function () {
@@ -43,32 +46,62 @@
               $scope.username = value;
               $scope.remember_me = true;
               $scope.$applyAsync();
-          }, function (error) { $scope.username = ""; }, 'username');
+          }, function (error) { $scope.username = ""; },
+            'username'
+          );
           secure_storage.get(function (value) {
               $scope.password = value;
               $scope.remember_me = true;
               $scope.$applyAsync();
-          }, function (error) { $scope.password = ""; }, 'password');
+          }, function (error) { $scope.password = ""; },
+            'password'
+          );
+          secure_storage.get(function (value) {
+              for (var i = 0; i < $scope.protocols.length; i++) {
+                if ($scope.protocols[i].id === JSON.parse(value).id) {
+                    $scope.protocol = $scope.protocols[i];
+                    break;
+                }
+              }
+              $scope.remember_me = true;
+              $scope.$applyAsync();
+          }, function (error) { $scope.protocol = $scope.protocols[1]; },
+            'protocol'
+          );
           secure_storage.get(function (value) {
               $scope.server = value;
               $scope.remember_me = true;
               $scope.$applyAsync();
-          }, function (error) { $scope.server = ""; }, 'server');
+          }, function (error) { $scope.server = ""; },
+            'server'
+          );
       }
 
       function storeCredentials() {
           secure_storage.set(
             function (key) { console.log('Set ' + key); },
             function (error) { console.log('Error ' + error); },
-            'username', $scope.username);
+            'username',                       // key
+            $scope.username                   // value
+          );
           secure_storage.set(
             function (key) { console.log('Set ' + key); },
             function (error) { console.log('Error ' + error); },
-            'password', $scope.password);
+            'password',                       // key
+            $scope.password                   // value
+          );
           secure_storage.set(
             function (key) { console.log('Set ' + key); },
             function (error) { console.log('Error ' + error); },
-            'server', $scope.server);
+            'protocol',                       // key
+            JSON.stringify($scope.protocol)   // value
+          );
+          secure_storage.set(
+            function (key) { console.log('Set ' + key); },
+            function (error) { console.log('Error ' + error); },
+            'server',                         // key
+            $scope.server                     // value
+          );
       }
 
   }]);
